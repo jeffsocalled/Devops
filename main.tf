55866b89608a9360b31fec78c78993c39ef57bee
G Praveen Kumar <84589796+jeffsocalled@users.noreply.github.com>
2023-10-01 20:43:23 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required providers {
+    aws = {
+      source = “hashicorp/aws”
+      version = ”~> 4.16”
+    }
+  }
+  required version = “>= 1.2.1”
+}
+provider “aws” {
+  region = “us-west-2”
+}
+
+resource “aws_instance” “app_server” {
+  ami	=	“ami-830c94e3”
+  instance_type	=	“t2.micro”
+
+  tags	=	{
+    Name		=	“PraveensAppServerInstance”
+  }
+}
